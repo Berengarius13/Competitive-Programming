e558ab2fcb071e50a58ca406a092c68bfebd7987
Piyush <78652675+Berengarius13@users.noreply.github.com>
2021-10-17 10:34:22 +0000

    Time: 40 ms (88.35%), Space: 14.5 MB (89.02%) - LeetHub

--- intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -10,25 +10,99 @@ class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode* ans;
-        ListNode* s;
         ans = NULL;
-        s = headB;
+        int a =0; 
+        int b =0;
+        ListNode* tA = headA;
+        ListNode* tB = headB;
+//         ListNode* s;
+        
+//         s = headB;
+//         while (headA !=NULL && headB !=NULL){
+//             if(headA != headB)
+//             {
+//                 headB= headB->next;
+//                 if(headB == NULL ){
+//                     headA = headA->next;
+//                     headB=s;
+//                 }
+//             }
+//             else 
+//             {
+//                 ans =headA;
+//                 break;
+//             }
+            
+//         }
+//         while (tA !=NULL || tB !=NULL){
+//             if(tA!=NULL){
+//                 a++;
+//                 tA= tA->next;
+//             }
+//             if(tB!=NULL){
+//                 b++;
+//                 tB= tB->next;
+                
+//             }
+//             if(tA==NULL && tB == NULL)
+//                 break;
+//         }
+        while(tA!=NULL){
+            a++;
+            tA=tA->next;
+        }
+        while(tB!=NULL){
+            b++;
+            tB=tB->next;
+        }
+        if(a>b)
+        {
+            for(int i=0; i<a-b;i++){
+                headA=headA->next;
+            }
+        }
+        else if (b>a) {
+            for(int i=0; i<b-a;i++)
+                {headB=headB->next;}
+        }
+        // while (true){
+        //     tA = tA->next;
+        //     tB = tB->next;
+        //     if(tA==NULL)
+        //     {
+        //         while(true){
+        //             tB = tB->next;
+        //             headA = headA->next;
+        //             if(tB==NULL)
+        //                 break;
+        //         }
+        //     }
+        //     else if(tB==NULL)
+        //     {
+        //         while(true){
+        //             tA = tA->next;
+        //             headB = headB->next;
+        //             if(tA==NULL)
+        //                 break;
+        //         }
+        //     }
+        //     if(tA == NULL && tB == NULL)
+        //         break;
+        // }
+        
         while (headA !=NULL && headB !=NULL){
-            if(headA != headB)
-            {
-                headB= headB->next;
-                if(headB == NULL ){
-                    headA = headA->next;
-                    headB=s;
-                }
+            if(headA!=headB){
+                headA=headA->next;
+                headB=headB->next;
             }
             else 
             {
-                ans =headA;
+                ans = headA;
                 break;
             }
-            
         }
+        
         return ans;
+        
     }
 };
