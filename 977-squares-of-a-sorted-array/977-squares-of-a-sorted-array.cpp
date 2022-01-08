50f8723751c5ac366725de1e6455fe374af43efd
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-01-09 02:58:56 +0530

    Time: 20 ms (98.18%), Space: 27.8 MB (5.37%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,14 +1,50 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(auto &num: nums)
-            num = abs(num);
+
         
-        sort(nums.begin(), nums.end());
+        vector<int> temp, ans;
+        for(int i =0; i< nums.size(); i++)
+        {      if(nums[i]<0)
+                    temp.push_back(nums[i]);
+                
+         } 
+        for(int i =0; i < temp.size()/2; i++)
+                swap(temp[i], temp[temp.size()-i-1]);
         
-        for(auto &num: nums)
-            num = num*num;    
+        for(auto &t: temp)
+            t = abs(t);
+        
+         
+        int m = nums.size();
+        int n = temp.size();
+        int i =n; 
+        int j =0;
+        while(i<m && j <n){
+            if(nums[i]>temp[j])
+            {
+                ans.push_back(temp[j]);
+                j++;
+            }
+            else{
+                ans.push_back(nums[i]);
+                i++;
+            }
+        }
+        while(i<m){
+            ans.push_back(nums[i]);
+            i++;
+        }
+        while(j<n){
+            ans.push_back(temp[j]);
+            j++;
+        }
+        
+        
+        
+        for(auto &an: ans)
+            an = an*an;    
        
-       return nums;     
+       return ans;     
     }
 };
