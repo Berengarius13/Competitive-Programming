29f88873a2fc7468fc921b497ea1d406fda4c27c
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-01-09 19:09:39 +0530

    Time: 4 ms (49.93%), Space: 9.1 MB (27.88%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,28 +1,43 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> ans;
-        int i =0; 
-        int j =0; 
-        while(i<m && j <n){
-            if(nums1[i]>nums2[j])
-            {
-                ans.push_back(nums2[j]);
+        
+        if(m == 0){
+            nums1 = nums2;
+            nums2.clear(); 
+        }
+
+        int i =m-1; 
+        int j = m+n-1;
+        while(i>=0){
+            swap(nums1[i], nums1[j]);
+            i--;
+            j--;
+        }
+        int k =0;
+        j =0;
+        i = n;
+        while(j <n && i < m+n){
+            if(nums1[i]>nums2[j]){
+                nums1[k] = nums2[j];
+                k++;
                 j++;
             }
             else{
-                ans.push_back(nums1[i]);
+                nums1[k] = nums1[i];
+                k++;
                 i++;
+            }            
+        }
+        while(j < n){
+                nums1[k] =nums2[j];
+                k++;
+                j++;
             }
-        }
-        while(i<m){
-            ans.push_back(nums1[i]);
-            i++;
-        }
-        while(j<n){
-            ans.push_back(nums2[j]);
-            j++;
-        }
-        nums1 = ans;
+        while(i<m+n){
+                nums1[k],nums1[i];
+                i++;
+                k++;
+            }
     }
 };
