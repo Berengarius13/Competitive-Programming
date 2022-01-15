29cc01a3f660565f12545b6f499faf336ced1301
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-01-15 22:11:50 +0530

    Time: 4 ms (75.80%), Space: 9.4 MB (78.69%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -2,16 +2,62 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         vector<int> temp; 
-        for(int i = 0; i< matrix.size(); i++){
-            for(int j =0; j < matrix[0].size(); j++)
-                temp.push_back(matrix[i][j]);
+        bool ans=false;
+        int s = 0; 
+        int e = matrix.size()-1;
+        int n = matrix[0].size()-1;
+        int v{0};
+        int m;
+        if(matrix.size()==1 && matrix[0].size()==1){
+            if(matrix[0][0]==target)
+                return true;
+             else
+                return false;
+        }       
+        if(matrix.size()!=1){
+        while(s<=e)
+        {   
+           m = (s+e)/2;
+            if(target>=matrix[m][0]&& target<=matrix[m][n]){
+                v = m;
+                break;
+            }
+            else if (target< matrix[m][0] && target < matrix[m][n]){
+                m = e; e--;
+            }
+            else if (target > matrix[m][0] && target > matrix[m][n]){
+                m = s; s++;
+            }
         }
-        int s =0; 
-        int e = matrix.size();
-        auto it = find(temp.begin(), temp.end(), target);
-        if(it != temp.end())
-            return true;
-        else
-            return false;
+    }
+        
+        for(int i =0; i<matrix[0].size(); i++ ){
+            if(matrix[v][i] == target){
+                ans= true; break;}
+        }
+        
+        return ans;
     }
 };
+      
+        // int s = 0; 
+        // int e = nums.size()-1;
+        // int ans = -1;
+        // if(e ==0){
+        //     if(nums[0]== target)
+        //         return 0;
+        // }
+        // while(s<=e)
+        // {
+        //     int m = (s+e)/2; 
+        //     if(target < nums[m]){
+        //         m--; 
+        //         e=m;
+        //     }
+        //     else if(target > nums[m]){
+        //         m++;
+        //         s =m;
+        //     }
+        //     else
+        //         return m;
+        // }
