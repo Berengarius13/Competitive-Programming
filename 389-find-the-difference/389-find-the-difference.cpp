ca0c66899303e9b5ae1e2ecf721e6c2b9b98d8d3
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-01-21 12:52:01 +0530

    Time: 4 ms (70.00%), Space: 6.7 MB (66.97%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        char ans; 
-        unordered_map<char, int> m1; 
-        unordered_map<char, int> m2;
-        /*
-        * Remember this technique to store frequency in maps
-        * Remember map is initialize to zero automatically
-        */
-        for(auto c: s)
-            m1[c] = m1[c]+1;
-        for(auto c: t)
-            m2[c] = m2[c] +1;
-        for(auto pair: m2){
-            if(pair.second != m1[pair.first])
-            {ans = pair.first; break;}
-        }
+        // Maps method
+//         char ans; 
+//         unordered_map<char, int> m1; 
+//         unordered_map<char, int> m2;
+//         /*
+//         * Remember this technique to store frequency in maps
+//         * Remember map is initialize to zero automatically
+//         */
+//         for(auto c: s)
+//             m1[c] = m1[c]+1;
+//         for(auto c: t)
+//             m2[c] = m2[c] +1;
+//         for(auto pair: m2){
+//             if(pair.second != m1[pair.first])
+//             {ans = pair.first; break;}
+//         }
         
-        return ans;
+//         return ans;
+        // Bitwise method
+        int b = 0; 
+        for(auto c:s)
+            b = b^c;
+        for(auto c:t)
+            b = b^c;
+        return b;
         
     }
 };
