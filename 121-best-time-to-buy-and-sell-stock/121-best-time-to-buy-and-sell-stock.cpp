7b3e2c80e134f85b976c08f28c709f96712baadb
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-01-23 22:44:37 +0530

    Time: 170 ms (40.33%), Space: 93.3 MB (53.32%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -7,11 +7,11 @@ public:
         while(i+1 < prices.size()){
             
             maxi = maxi + prices[i+1] - prices[i];
+            
             ans = max(maxi, ans);
-            if(maxi < 0){
-                ans = max(maxi, ans);
-                maxi = 0;
-            }
+            
+            if(maxi < 0) maxi = 0;
+            
             i++;
         }
         if(ans < 0) return 0;
