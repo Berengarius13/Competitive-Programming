45226cda11e0eb33dc1c0857a234f242660d24b1
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-01-23 15:32:39 +0530

    Time: 27 ms (20.44%), Space: 13.1 MB (82.08%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -23,3 +23,25 @@ public:
     
     }
 };
+/*
+* O(m+n) memory solution
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<int> arrm (m,0);
+        vector<int> arrn (n,0);
+        
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(matrix[i][j] ==0){
+                    arrm[i] = 1;
+                    arrn[j] = 1;
+                }
+            }
+        }
+        for(int i = 0; i < m ; i++){
+            for(int j = 0 ; j < n; j++){
+                if(arrm[i] == 1 || arrn[j] ==1)
+                    matrix[i][j] = 0;
+            }
+        }
+*/
