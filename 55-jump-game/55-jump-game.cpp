09bdd418977baae622b628a438828d037e8bae22
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-01-28 02:43:55 +0530

    Time: 1188 ms (5.01%), Space: 51.4 MB (5.52%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canReach(vector<int>& nums,int i,vector<int> &m){
+        if(m[i]!=-1){
+            return m[i];
+        }
+        if(i==nums.size()-1){
+            return true;
+        }
+        for(int j=1;j<=nums[i]&&j+i<nums.size();j++){
+            if(canReach(nums,i+j,m)){
+                return m[i]=1;
+            }
+        }
+        return m[i]=0;
+    }
+    bool canJump(vector<int>& nums) {
+        vector<int> m(nums.size(),-1);
+        return canReach(nums,0,m);
+        
+    }
+};
