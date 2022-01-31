953abcc167e7729292cc759332402443b0a6fb43
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-01-31 16:24:30 +0530

    Time: 64 ms (70.93%), Space: 48.3 MB (80.39%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    /* Greedy Approach
+    * We create a "Goal" pointer and set it end.
+    * We check is it possible to reach from "goal -1", 
+    * How do we check? Well we loot at "max jump on 'goal-1'", then use that condition.
+    * If yes we shift on place less until it reaches the first position
+    * BTW we should start it from i-1, but it works either way, i-1 requires an extra condition
+    */
+    bool canJump(vector<int>& nums) {
+        int goal = nums.size()-1;
+        for(int i = goal; i >=0; i--){
+            if(i + nums[i] >= goal)
+                goal = i;
+        }
+        return goal == 0; 
+    }
+};
+
+/*
+* Given below is the DP approach
+class Solution {
+public:
     bool canReach(vector<int>& nums,int i,vector<int> &m){
         if(m[i]!=-1){
             return m[i];
@@ -20,3 +41,4 @@ public:
         
     }
 };
+*/
