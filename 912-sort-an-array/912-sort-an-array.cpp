b300eb25954922c91a2654efac0e0cdcc78d7f54
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-02-25 14:38:55 +0530

    Time: 101 ms (47.48%), Space: 28.5 MB (81.94%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
-    void sort(vector<int> &nums, int i, int n, vector<int> &ans){
-        if(i+1 == n){
-            if(nums[i]>nums[n]) swap(nums[i],nums[n]);
-            return;
-        }
-        if(i==n) return;
+    // Heapify 
+    // n = We need to difine how much of heap we want to create
+    void heapify(vector<int> &nums,int n,int i){
         
-        sort(nums, i, (n+i)/2, ans);
-        sort(nums, ((n+i)/2)+1, n, ans);
-        int a=i; 
-        int m = (i+n)/2;
-        int b = m+1;
-        while(a<=m && b <=n){
-            if(nums[a]>nums[b]){
-                ans.push_back(nums[b]);
-                b++;
-            }
-            else{
-                ans.push_back(nums[a]);
-                a++;
-            }
+        int largest = i;
+        int left = 2*i + 1; 
+        int right = 2*i + 2; 
+        if(left < n && nums[largest]< nums[left]) 
+            largest = left;
+        if(right < n && nums[largest] < nums[right])
+            largest = right;
+        if(i != largest){
+            swap(nums[largest], nums[i]);
+            heapify(nums, n,largest);
         }
-        while(a<=m){
-            ans.push_back(nums[a]);
-            a++;
-        }
-        while(b<=n){
-            ans.push_back(nums[b]);
-            b++;
-        }
-        m=0;
-        for(int v = i; v<=n; v++){
-            swap(ans[m],nums[v]);
-            m++;
-        }
-        ans.clear();
-        return;
     }
     vector<int> sortArray(vector<int>& nums) {
-        vector<int> ans;
-        sort(nums, 0, nums.size()-1, ans);
+        for(int i = nums.size()/2-1; i >= 0; i--){
+            heapify(nums, nums.size(),i);
+        }
+        for(int i = nums.size()-1;  i >=0; i--){
+            swap(nums[0],nums[i]);
+            heapify(nums, i,0);
+        }
         return nums;
+            
     }
 };
