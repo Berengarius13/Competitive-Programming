922f0a2a09a739a618cd511cbb74f62b64d18271
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-05-21 02:48:36 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (48.03%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int max_sum;
+    int rf(vector<int> &nums, int i,vector<int> &memo){
+        if(i != -2)
+            if(memo[i] != -1) return memo[i];
+        int n = nums.size();
+        if(i == n-1) return nums[i];
+        if(i == n-2) return nums[i];
+        int doub_j = 0; int trip_j = 0;
+        if(i + 2 < n)
+            doub_j = rf(nums, i+2, memo);
+        if(i+3 < n)
+            trip_j = rf(nums, i+3, memo);
+        
+        if(i == -2) return max(doub_j, trip_j);
+        memo[i]  = nums[i] + max(doub_j, trip_j);
+        return nums[i] + max(doub_j, trip_j);
+    }
+    int rob(vector<int>& nums) {
+        max_sum = 0;
+        int maxi = nums[0];
+        vector<int> memo(nums.size()+1, -1);
+        max_sum =rf(nums, -2, memo);
+        return max_sum;
+    }
+};
