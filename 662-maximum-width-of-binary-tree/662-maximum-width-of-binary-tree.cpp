849f21426d79fd9f9f910c816bd8f86a1c5ec45e
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-05-25 03:28:59 +0530

    Time: 25 ms (6.10%), Space: 19.7 MB (5.11%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -1,59 +1,50 @@
 class Solution {
-public:
-    int widthOfBinaryTree(TreeNode* root) {
-        if(root == NULL) // if root is null 
-            return 0;   // simply return zero
-        
-        int ans = 0; // variable to store answer
-        
-        // queue for level order traveral
-        queue<pair<TreeNode*, int>> q; // pair contain { node, index}
-        
-        q.push({root, 1}); // intially push root node 
-        
-        // Implementing BFS
-        while(q.empty() == false) // until queue is not empty
-        {
-            int size = q.size(); // take size of the queue
-            
-            // tells us minimum index at particular level
-            int minAtLevel = q.front().second; 
-            
-            // declaring minimum and maximum variable used for finding width
-            int mn, mx;
-            
-            // traverse from the queue
-            for(int i= 0; i < size; i++)
-            {
-                // changes index of level by decreasing minimum index
-                int curr_index = q.front().second - minAtLevel + 1; //+ 1 because we are using 1 based indexing,
-                
-                // take out current node
-                TreeNode* node = q.front().first;
-                q.pop(); // pop from the queue
-                
-                // remember the formula we discussed
-            //width = (maximum index at level) - (minimum index at level) + 1
-                
-                if(i == 0) 
-                    mn = curr_index; // minimum index at level
-                
-                if(i == size - 1)
-                    mx = curr_index; //maximum index at level
-                
-                // push left and right for further calculation
-				
-                if(node -> left != NULL) //2   * i
-                    q.push({node -> left, 2LL * curr_index});
-                
-                if(node -> right != NULL) //2   * i  + 1
-                    q.push({node -> right, 2LL * curr_index + 1});
-                   
-            }
-            ans = max(ans, mx - mn + 1); // update our answer
+    public:
+    #define ll unsigned long long 
+    void end(TreeNode *root,unordered_map<ll,ll> &m,ll h,ll c){
+        if(!root){
+            return ;
         }
+        if(m.find(h)==m.end()){
+            m.insert({h,c});
+        }
+        else
+        {
+            if(m[h]<c){
+                m[h]=c;
+            }
+        }
+        end(root->left,m,h+1,2*c);
+        end(root->right,m,h+1,2*c+1);
         
-        return ans; // finally return our answer
+    }
+    void strt(TreeNode *root,unordered_map<ll,ll> &m,ll h,ll c){
+        if(!root){
+            return ;
+        }
+        if(m.find(h)==m.end()){
+            m.insert({h,c});
+        }
+        else
+        {
+            if(m[h]>c){
+                m[h]=c;
+            }
+        }
+        strt(root->left,m,h+1,2*c);
+        strt(root->right,m,h+1,2*c+1);
         
+    }
+    int widthOfBinaryTree(TreeNode* root) {
+        unordered_map<ll,ll> s,e;
+        end(root,e,0,0);
+        strt(root,s,0,0);
+        ll ans=0;
+        for (auto i = s.begin(); i!=s.end(); i++)
+        {
+            ans=max(ans,e[i->first]-i->second+1);
+        }
+        return int(ans);
+
     }
 };
