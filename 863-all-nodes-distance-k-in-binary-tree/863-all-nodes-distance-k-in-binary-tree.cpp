7e496fa3ec7505ec218601dcb71a69d45627cc86
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-05-27 01:54:17 +0530

    Time: 3 ms (93.89%), Space: 12.5 MB (79.49%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -7,6 +7,15 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+/*
+* Decide_dfs, returns the value of target_i, to previsous recursion calls
+* Dfs function, does depth first search and find the value at given distance and inserts in         answer
+* Main idea is that target we will remember height of target node.
+* From target node do dfs, initial distance i = 0, and all at k are added
+* The first parent of will be at 1 distance from target, thus we will add 1 to initial distanc
+  thus starting ditance for dfs will be i+1 = 1
+* This continues, as we go to parent of parent of target, intial distance changes to i+2.
+*/
 class Solution {
 public:
     vector<int> ans;
@@ -15,9 +24,12 @@ public:
         
         int a = decide_dfs(node->left, target, i+1, k);
         int b = decide_dfs(node->right, target, i+1, k);
+        
         if(b){
-            if(b-i == k) ans.push_back(node->val);
+            if(b-i == k) ans.push_back(node->val); // Edge case, if parent is at k distance
             dfs(node->left, b-i+1, k);
+            // If b is not 0, thus we have already done dfs on b, thus we do dfs on a branch side
+            // Remember to edit the inital distance
         }
         if(a){
             if(a-i == k)ans.push_back(node->val);
@@ -27,8 +39,10 @@ public:
             dfs(node, 0, k);
             return i;
         }
-        return a+b;
+        return a+b; // If a+b = 0, target is not found yet!
+                    // If a+b = 1, target if found, and remeber to not do dfs again on that                              branch
     }
+    // A function that simply remembers initial distance, and remembers nodes at k distance.
     void dfs(TreeNode* node, int i, int &k){
         if(node == NULL) return;
         if(i == k)
