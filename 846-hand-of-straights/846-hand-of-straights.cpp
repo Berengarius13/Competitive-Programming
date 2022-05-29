e0133a1bd36e023d3b31e1aa27d2e21cff7c25af
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-05-29 07:11:37 +0530

    Time: 108 ms (45.58%), Space: 28.3 MB (35.94%) - LeetHub

--- 846-hand-of-straights/846-hand-of-straights.cpp
+++ 846-hand-of-straights/846-hand-of-straights.cpp
@@ -1,3 +1,12 @@
+/**
+ * Remember why we are sorting
+ * Because Sorting we don't need to check previous elements
+ * If it starts from 4, then next k elements will be 4, 5, 6, 7 .. k elements
+ * When you use them just reduce their frequency
+ * If frequency is less than zero, aka we need the element and it is not there
+ * Than return false
+ */
+
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
@@ -10,7 +19,6 @@ public:
             while(mp[key]){
                 
                 for(int i = key; i < key+groupSize; i++){
-                    
                     mp[i]--;
                     if(mp[i] < 0)
                         return false;
