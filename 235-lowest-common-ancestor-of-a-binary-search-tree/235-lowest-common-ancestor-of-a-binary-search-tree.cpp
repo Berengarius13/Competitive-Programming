ecd569aea28e37b5f644578539d3067e88e00e1f
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-01 01:44:33 +0530

    Time: 33 ms (78.13%), Space: 23.2 MB (63.97%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -1,43 +1,24 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
+ * In bst during traversal you will always find that 
+   left is smaller than root and right is larger than root
+ * Using this property we can conclude that
+ * If root is greater than p and root is smaller than q then we have our answer
+ * Or if root is q and and p is greater than q
+ * Or if root is p and q is lesser than p
+ * Inverse of above is that 
+ * If root is greater than p and q than lsa is in left subtree
+ * If root is smaller than p and q lsa is in rihgh subtree
  */
 
 class Solution {
 public:
-    TreeNode* ans;
-    bool dfs(TreeNode* node, TreeNode* p, TreeNode* q){
-        if(node == NULL) return false;
-        
-        bool a = dfs(node->left, p, q);
-        bool b = dfs(node->right, p, q);
-        // If there is an lca in both subtree then this case
-        if(a&b){
-            ans = node; return false;
-        }
-        // If there is an lca in one tree then this case, compare with parent
-        if(a|b){
-            if(node->val == p->val || node->val == q->val){
-                ans = node; return false;
-            }
-        }
-        // return true if node is equal to either of them
-        if(node->val == p->val || node->val == q->val) return true;
-        
-        // In any case return a|b;
-        return a|b;
-    }
-    TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q) {
-        
-        dfs(root, p, q);
-        return ans;
-    }    
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return lca(root, p,q);
+        if ((root -> val > p -> val) && (root -> val > q -> val)) {
+            return lowestCommonAncestor(root -> left, p, q);
+        }
+        if ((root -> val < p -> val) && (root -> val < q -> val)) {
+            return lowestCommonAncestor(root -> right, p, q);
+        }
+        return root;
     }
 };
