f0c8fdbb81b6a39152bd65b31b342c23796ff702
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-05 13:28:00 +0530

    Time: 226 ms (100.00%), Space: 83 MB (50.00%) - LeetHub

--- 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.cpp
+++ 2294-partition-array-such-that-maximum-difference-is-k/2294-partition-array-such-that-maximum-difference-is-k.cpp
@@ -6,17 +6,19 @@
  */
 class Solution {
 public:
-      int partitionArray(vector<int>& A, int k) {
-        sort(A.begin(), A.end());
-        int res = 1, mn = A[0], mx = A[0];
-        for (int& a: A) {
-            mn = min(mn, a);
-            mx = max(mx, a);
-            if (mx - mn > k) {
-                res++;
-                mn = mx = a;
+    int partitionArray(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int mn = nums[0];
+        int mx = nums[0];
+        int count = 1; // Because we are counting when we are at next case
+        for(int i = 0; i < nums.size(); i++){
+            mn = min(mn, nums[i]);
+            mx = max(mx, nums[i]);
+            if((mx-mn) > k){
+                count++;
+                mn = mx = nums[i];
             }
         }
-        return res;
+        return count;
     }
 };
