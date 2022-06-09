1bf3635283926847bc9e4929b4b0770061bdf064
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-09 20:49:14 +0530

    Time: 1253 ms (5.01%), Space: 201.7 MB (5.02%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool knapsack(vector<int> &nums, int &total, int i, int sum, vector<vector<int>> &memo){
+        if(memo[i][sum] != -1) return memo[i][sum];
+        if(total-sum == sum) return true;
+        if(i == nums.size()) return false;
+        
+        bool a = knapsack(nums, total, i+1, sum + nums[i], memo);
+        bool b = knapsack(nums, total, i+1, sum, memo);
+        
+        memo[i][sum] = a|b;
+        return a|b;
+    }
+    bool canPartition(vector<int>& nums) {
+        int total = 0;
+        for(auto &num : nums)
+            total += num;
+        vector<vector<int>> memo(nums.size()+2, vector<int> (total+2, -1));
+        return knapsack(nums, total, 0, 0, memo);
+    }
+};
