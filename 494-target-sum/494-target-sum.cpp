17cf488ebb9f1435f685c2ac8b3b0d3f20032f7d
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-09 17:28:44 +0530

    Time: 438 ms (29.63%), Space: 62.9 MB (8.09%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int knapsack(vector<int>& nums, int &target, int i, int sum, map<int, map<int,int>> &mp){
+        if(mp.find(i) != mp.end()){
+            if(mp[i].find(sum) != mp[i].end()) return mp[i][sum];
+        }
+        if(i == nums.size() && sum == target) return 1;
+        if(i == nums.size()) return 0;
+    
+        int left = knapsack(nums, target, i+1, sum+nums[i], mp);
+        int right = knapsack(nums, target, i+1, sum-nums[i], mp);
+        
+        mp[i][sum] = left+right;
+        return left+right;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        map<int, map<int,int>> mp;
+        return knapsack(nums, target, 0, 0, mp);
+    }
+};
