b33f862111842016301367b8a1b357415f80d19e
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-14 17:22:38 +0530

    Time: 52 ms (10.90%), Space: 16.3 MB (14.06%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void dfs(vector<int>& nums, int target, int begin,vector<int> temp){
+        if(target < 0) return;
+        if(target == 0){
+            ans.push_back(temp); return;
+        }
+        
+        for(int i = begin; i < nums.size(); i++){
+                if(i == begin || nums[i] != nums[i-1]){
+                temp.push_back(nums[i]);
+                dfs(nums, target-nums[i], i+1, temp);
+                temp.pop_back();
+            }
+        }
+        
+    }
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<int> temp;
+        sort(candidates.begin(), candidates.end());
+        dfs(candidates, target, 0,temp);
+        return ans;
+    }
+};
