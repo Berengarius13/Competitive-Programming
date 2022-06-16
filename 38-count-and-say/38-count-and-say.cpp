21252b7b402ce079ec25780b318d7df028a5409b
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-16 23:09:52 +0530

    Time: 8 ms (73.05%), Space: 6.7 MB (42.13%) - LeetHub

---
+++ 38-count-and-say/38-count-and-say.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string str = "1";
+        for(int i = 1; i < n; i++){
+            int a = 0; int b = 0; int count = 0; string temp;
+            while(b < str.size()){
+                if(str[a] == str[b]){
+                    count++; b++;
+                }
+                else{
+                    temp += to_string(count);
+                    temp += str[a];
+                    a = b; count = 0;
+                }
+            }
+            temp += to_string(count);
+            temp += str[a];
+            // cout<<temp<<" ";
+            str = temp;
+        }
+        return str;
+    }
+};
