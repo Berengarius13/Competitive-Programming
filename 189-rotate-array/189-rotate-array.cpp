999ddc21a9d5422fde99174274976e0f3d02f6c6
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-16 18:58:06 +0530

    Time: 57 ms (18.64%), Space: 25 MB (81.17%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,29 +1,106 @@
+/* * 
+ * Reverse whole array
+ * Reverse first k
+ * Reverse reverse remaining
+ */
 class Solution {
 public:
-//     void Rotate(vector<int> &nums, int begin, int k, int count){
-//         int n = nums.size();
-//         // if(n-1-begin < k) return;
-//         if(count == 0) return;
+    void reverse(vector<int> &nums, int s, int e){
+        int m = (s+e)>>1;
+        while(s < e){
+            swap(nums[s], nums[e]);
+            s++; e--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size()-1;
+        if(k == 0) return;
+        k = k % nums.size();
+        reverse(nums,0, n);
+        reverse(nums,0, k-1);
+        reverse(nums,k, n);
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class Solution {
+public:
+    void Rotate(vector<int> &nums, int begin, int k, int count){
+        int n = nums.size();
+        // if(n-1-begin < k) return;
+        if(count == 0){
+           return;
+        }
         
-//         for(int i = n-k ; i < nums.size(); i++){
-//             swap(nums[i], nums[begin]);
-//             begin++;
-//         }
-//         Rotate(nums, begin, k, count-1);
-//     }
+        for(int i = n-k ; i < nums.size(); i++){
+            swap(nums[i], nums[begin]);
+            begin++;
+        }
+        // k = n-k;
+        Rotate(nums, begin, k, count-1);
+    }
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
         k = k % n;
-        // cout<<k;
-        // if(k){
-        //     int count = n/k;
-        //     Rotate(nums, 0, k, count);
-        // }
-        vector<int> ans;
-        for(int i = n-k; i < n; i++)
-            ans.push_back(nums[i]);
-        for(int i = 0; i < n-k; i++)
-            ans.push_back(nums[i]);
-        nums = ans;
+        if(k){
+            int count = n/k;
+            Rotate(nums, 0, k, count);
+        }
+        
     }
 };
+*/
+/*
+ int calculate (int i, int n, int k){
+        if(i < n-k)
+            return i + k;
+        else
+            return i - (n-k);
+    }
+    void rotate(vector<int>& nums, int k) {
+        int i = k;
+        int val = nums[k];
+        k = k % nums.size();
+        for(int j = 0; j  < k; j++){
+            
+            int f = calculate(i, nums.size(), k); 
+            cout<<f<<" ";
+            int temp = nums[f];
+            nums[f] = val;
+            
+            val = temp;
+            i = f;
+        }
+    }
+*/
