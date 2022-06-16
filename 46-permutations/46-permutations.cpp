046d3cf8008bec1df2129cd39b0eb42f721a1095
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-17 01:45:54 +0530

    Time: 12 ms (10.91%), Space: 7.9 MB (37.66%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void rearr(vector<int> &nums,int b){
-        if(b == nums.size()-1) {
+    void dfs(vector<int> &nums, int begin){
+        if(begin == nums.size()-1){
             ans.push_back(nums);
             return;
         }
-        for(int i = b; i < nums.size(); i++){
-            swap(nums[b], nums[i]);
-            rearr(nums, b+1);
-            // We reset the swap
-            swap(nums[b], nums[i]);
+        for(int i = begin; i < nums.size(); i++){
+            swap(nums[i], nums[begin]);
+            dfs(nums, begin+1);
+            swap(nums[i], nums[begin]);
         }
-        return;
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        rearr(nums,0);
+        dfs(nums, 0);
         return ans;
     }
 };
