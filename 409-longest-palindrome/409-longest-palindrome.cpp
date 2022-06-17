6b1b63bc7301bdc81b40a965408f6a588b397593
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-17 17:18:26 +0530

    Time: 5 ms (44.78%), Space: 6.7 MB (13.04%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        vector<int> count(256);
+        for (char c : s)
+            ++count[c];
+        int odds = 0;
+        for (int c : count)
+            odds += c & 1;
+        return s.size() - odds + (odds > 0);
+    }
+};
