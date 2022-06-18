a8fda2e14e346f73cd4efd3f8abb01b6d49ecdb5
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-19 01:59:31 +0530

    Time: 3 ms (88.81%), Space: 12.1 MB (36.67%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -2,27 +2,24 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         for(int i = nums.size()-2; i >= 0; i--){
-            int maxi = i+1;
+            int maxi = i+1; // Find maximum value after i
             for(int j = i+1; j < nums.size(); j++){
                 if(nums[maxi] < nums[j]){
                     maxi = j;
                 }
             }
+            
             if(nums[i] >= nums[maxi]){
-                cout<<1<<" ";
                 for(int j = i+1; j < nums.size(); j++)
                     swap(nums[j], nums[j-1]); // this will sort
             }
             else{
-                cout<<2<<" ";
                 for(int j = i+1; j < nums.size(); j++)
                     if(nums[j] > nums[i]){
                         swap(nums[j], nums[i]);
                         return;
                 }
             }
-            
         }
-      return;
     } 
 };
