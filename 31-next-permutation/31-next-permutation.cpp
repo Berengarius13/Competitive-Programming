122836c15c4e03127d7080577ca2032f2760fd1a
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-19 02:43:59 +0530

    Time: 7 ms (55.18%), Space: 12 MB (74.73%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,17 +1,41 @@
+/* *
+ * '|' is break point.
+ * |1234
+        -> 1|234
+                -> 12|34
+                        -> 123|4
+                        -> 124|3
+                -> 13|24
+                        -> 132|4
+                        -> 134|2
+                -> 14|23
+                        -> 142|3
+                        -> 143|2
+        -> 2|134
+                >> so on....
+ */
+/* *
+ * Making ovservation on above solution tree
+ * Lets assume => i | array
+ * If left of breakpoint aka i, is greatest
+   aka greater than last value, then we need to "step back"
+ * We will keep stepping back until we can step ahead
+   aka, we will swap i with it's just greater value
+ * Make simple observation that next of i is always sorted
+   if not than swap i with it's just greater value
+ */
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         for(int i = nums.size()-2; i >= 0; i--){
-            
             // If maximum value is greater than i value
-            // sort it with remaining value, aka put it in end
+            // Sort it with remaining value, aka put it in end
             // Cuz it is already sorted
             if(nums[i] >= nums[nums.size()-1]){
                 for(int j = i+1; j < nums.size(); j++)
                     swap(nums[j], nums[j-1]); // this will sort
             }
             // Swap the i value with just greater value
-            // We have our required array
             else{
                 for(int j = i+1; j < nums.size(); j++)
                     if(nums[j] > nums[i]){
