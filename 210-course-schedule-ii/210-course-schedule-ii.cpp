eb526a7ecc2052b7b82f28c630f27bb8471ecd04
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-19 03:14:14 +0530

    Time: 29 ms (61.12%), Space: 74.7 MB (5.09%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    typedef vector<vector<int>> graph;
+    graph make_graph(int &num, graph &pre){
+        graph g(num);
+        for(auto &it: pre){
+            g[it[1]].push_back(it[0]);
+        }
+        return g;
+    }
+    stack<int> stk;
+    bool dfs(graph &g, int node, vector<int> &vis, vector<int> par){
+        vis[node] = 1;
+        par[node] = 1;
+        for(auto &cnode : g[node]){
+            if(vis[cnode] == 0){
+                if(dfs(g, cnode, vis, par)) return true;
+            }
+            else if(par[cnode] == 1){
+                return true;
+            }
+            
+        }
+        stk.push(node);
+        return false;
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        graph g = make_graph(numCourses, prerequisites);
+        vector<int> vis(g.size(), 0);
+        vector<int> par(numCourses, 0);
+        vector<int> ans;
+        for(int i = 0; i < g.size(); i++){
+            if(vis[i] == 0)
+                if(dfs(g, i, vis, par))return ans;
+        }
+        while(!stk.empty()){
+            ans.push_back(stk.top()); stk.pop();
+        }
+        return ans;
+    }
+};
