f5403c1d1db971b66042cb84d28626fca3af24a7
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-19 02:23:33 +0530

    Time: 3 ms (88.81%), Space: 12 MB (93.05%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -8,11 +8,15 @@ public:
                     maxi = j;
                 }
             }
-            
-            if(nums[i] >= nums[maxi]){
+            // If maximum value is greater than i value
+            // sort it with remaining value, aka put it in end
+            // Cuz it is already sorted
+            if(nums[i] >= nums[nums.size()-1]){
                 for(int j = i+1; j < nums.size(); j++)
                     swap(nums[j], nums[j-1]); // this will sort
             }
+            // Swap the i value with just greater value
+            // We have our required array
             else{
                 for(int j = i+1; j < nums.size(); j++)
                     if(nums[j] > nums[i]){
