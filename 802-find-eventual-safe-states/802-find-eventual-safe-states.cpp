3c3cf894fe80778d1c355ecb8d0f1202eb1ec8d6
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-21 00:09:13 +0530

    Time: 294 ms (31.07%), Space: 47.8 MB (48.78%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> ans;
-    int dfs(vector<vector<int>> &graph, int node, vector<int> &vis){
+    void dfs(vector<vector<int>> &graph, int node, vector<int> &vis){
         vis[node] = 1;
         int is_end = 1;
         for(int cnode: graph[node]){
@@ -10,19 +10,14 @@ public:
                 dfs(graph, cnode, vis);
             }
         }
-        vis[node] = 2;
         if(is_end){
             vis[node] = 3;
-            cout<<node<<" ";
-            ans.push_back(node);
-            return 0;
         }
         for(auto &cnode : graph[node]){
-            if(vis[cnode] != 3) return 0;
+            if(vis[cnode] != 3) return;
         }
         vis[node] = 3;
         ans.push_back(node);
-        return 0;
     }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         vector<int> vis(graph.size(), 0);
