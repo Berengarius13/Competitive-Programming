f599a21995440541a30be84980546d982f650a77
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-20 13:39:03 +0530

    Time: 2771 ms (5.02%), Space: 566.1 MB (32.33%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.cpp
@@ -0,0 +1,55 @@
+#define Imposter ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+class Trie{
+public:
+    Trie *trieNodes [26];
+    bool isEnd=false;
+};
+class WordDictionary {
+public:
+    Trie* root;
+    WordDictionary() {
+        root= new Trie();
+    }
+    
+    void addWord(string word) {
+        Imposter
+        Trie* node=root;
+        for (char c:word){
+            if (node->trieNodes[c-'a']==NULL){
+                node->trieNodes[c-'a']=new Trie();
+            }
+            node=node->trieNodes[c-'a'];
+        }
+        node->isEnd=true;
+    }
+    bool solve(string word, Trie* node){
+        Imposter
+        for (int i=0;i<word.size();i++){
+            if (word[i]=='.'){
+                for (char c='a';c<='z';c++){
+                    if (node->trieNodes[c-'a']!=NULL && solve(word.substr(i+1),node->trieNodes[c-'a'])) return true;
+                }
+                return false;
+            }
+            else{
+                if (node->trieNodes[word[i]-'a']==NULL) return false;
+                node=node->trieNodes[word[i]-'a'];
+            }
+        }
+        if (node->isEnd) return true;
+        return false;
+        
+    }
+    bool search(string word) {
+        Imposter
+        Trie* node=root;
+        return solve(word,node);
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
