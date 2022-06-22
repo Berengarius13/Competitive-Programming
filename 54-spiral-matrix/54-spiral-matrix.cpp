4dd2b2e349c1053427fa4449086c4487a9978e86
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-22 23:46:42 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (29.73%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -1,3 +1,18 @@
+/**
+ * Basic DFS go like this
+ * Right (then again you try to go right but can't so)
+ * Down (then try right, but can't, then down, but can't so)
+ * Left (then right can't, down can't, left can't so)
+ * Up but.. just as you go just one step up you will go 
+   immediately right, cuz in recursion is called again.
+ * That's not what we want, we want it to go as up as possible
+ * Solution? we will return "true" from last left call
+ * That is cue to go as up as as posible
+ * Thus we run a for loop, reducing i as much as possible
+ * Then simply call the dfs for RIGHT from there
+ * Simple this whole cycle repeats
+ */
+
 class Solution {
 public:
     vector<int> ans;
@@ -21,28 +36,6 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int i = 0;
         int j = 0;
-    // {
-    //     while(j < matrix[0].size() && matrix[i][j] != -300){
-    //         ans.push_back(matrix[i][j]); matrix[i][j] = -300;
-    //         j++;
-    //     }
-    //     j--;
-    //     while(i < matrix.size() && matrix[i][j] != -300){
-    //         ans.push_back(matrix[i][j]); matrix[i][j] = -300;
-    //         i++;
-    //     }
-    //     i--;
-    //     while(j >= 0 && matrix[i][j] != -300){
-    //         ans.push_back(matrix[i][j]); matrix[i][j] = -300;
-    //         j--;
-    //     }
-    //     j++;
-    //     while(i >= 0 && matrix[i][j] != -300){
-    //         ans.push_back(matrix[i][j]); matrix[i][j] = -300;
-    //         i--;
-    //     }
-    //     i++;
-    // }    
         dfs(matrix, i, j);
         return ans;
     }
