d91c3b1a7bb957aaf79454907b4e5ac77cfb6019
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-06-23 15:18:59 +0530

    Time: 122 ms (59.67%), Space: 58.9 MB (81.07%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -1,3 +1,83 @@
+/*
+* Formula is l*h or (j-i) * min(h[i], h[j])
+* Pick up the best bet, the boundaries of container as then
+  we will have maximum length
+* How to proceed then? reduce lenght by one and discard the shorter
+  wall and pick the wall next to it
+* We don't need to check all cases of len-1 because we are discarding
+  shorter wall hence it will be definatly shorter
+when ai < aj, if we move j to the left:
+1. the length on x-axis will definitly decrease
+2. if a(j-1) > ai, the area will be ai * length on x-axis which is smaller than original area
+3. if a(j-1) < ai, the area will be a(j-1) * length on x-axis which is also smaller than original area
+so moving j to the left won't give us a larger area, we can only move i to the right to get a possible larger area.
+// Proof by contradiction
+*/
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int max_area = 0;
+        int i = 0; int j = height.size()-1;
+        while(i < j){
+            int area = (j-i)*min(height[i], height[j]);
+            max_area = max(max_area, area);
+            if(height[i] > height[j])
+                j--;
+            else 
+                i++;
+        }
+        return max_area;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+// O(NlogN) solution using priority queue and map
 class Solution {
 public:
     int maxArea(vector<int>& height) {
@@ -25,3 +105,4 @@ public:
     
     }
 };
+*/
