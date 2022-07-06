97b42c3ba9053c2ee82e5c78d837bbfd5078afc9
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-07-07 03:58:36 +0530

    Time: 20 ms (15.68%), Space: 5.9 MB (73.52%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long i = 1; long long val = 1;
+        long i = 1; long val = 1;
         while(val <= x){
             i++;
             val = i*i;
