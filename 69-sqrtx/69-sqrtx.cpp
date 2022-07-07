bff0db95197d9b928408c69fd56a0b86358d067a
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-07-08 01:19:52 +0530

    Time: 8 ms (27.41%), Space: 6 MB (25.09%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,6 +1,38 @@
 class Solution {
 public:
     int mySqrt(int x) {
+        long long s = 0, e = INT_MAX, ans = 0, m;
+        
+        while(s <= e){
+            m = s + ((e-s)>>1);
+            
+            if(m*m <= x){
+                ans = m;
+                s = m+1;
+            }
+            else
+                e = m-1;
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+class Solution {
+public:
+    int mySqrt(int x) {
         long i = 1; long val = 1;
         while(val <= x){
             i++;
@@ -9,3 +41,4 @@ public:
         return i-1;
     }
 };
+*/
