de300c78c542ba7f0e0ec7e825b57ffef3c61cad
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-07-13 18:26:24 +0530

    Time: 80 ms (79.09%), Space: 10 MB (67.39%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -1,9 +1,8 @@
 class Solution {
 public:
     bool dfs(vector<int> &nums, int i, vector<int> &square, int &k){
-        if(i == nums.size()){
+        if(i == nums.size())
             return true;
-        }
         
         
         for(int p = 0; p < square.size(); p++){
@@ -13,14 +12,15 @@ public:
             }
             square[p] -= nums[i];
         }
+        
         return false;
     }
     bool makesquare(vector<int>& nums) {
-        sort(nums.begin(), nums.end(), greater <>());
         int sum = 0;
         sum = accumulate(nums.begin(), nums.end(), sum);
         if(nums.size() < 4 || sum % 4 != 0 ) return false;
         sum = sum/4;
+        sort(nums.begin(), nums.end(), greater <>()); //
         vector<int> square(4, 0);
         if(dfs(nums, 0, square, sum)) return true;
         return false;
