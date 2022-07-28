25268e20170f37c62f403bd623e377bf78ca8d29
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-07-28 12:28:17 +0530

    Time: 16 ms (36.79%), Space: 9.6 MB (28.45%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.cpp
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -1,3 +1,21 @@
+/*
+* Use bit manupulation
+0001
+0001
+0001
+0010
+0010
+0010
+0101
+____
+0134
+If a number appears 3 times their set bits will also appears 3 times
+* Remember that to represent an answer we just need to remember the 
+set bits and thier position
+* So if total set bit count of a single column % 3 != 0 we have a set 
+bit from non repeating element their 
+* Use that information
+*/
 
 class Solution {
 public:
