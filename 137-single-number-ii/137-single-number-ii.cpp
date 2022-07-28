9527fdded8abe0d35b8ec76b76683d20c5ccf9b3
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-07-28 12:22:57 +0530

    Time: 7 ms (92.14%), Space: 9.5 MB (86.60%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0; i < 32; i++){
+            int count = 0;
+            for(auto num : nums)
+                if((num & (1 << i))) count++;
+            
+            if(count % 3 != 0) ans = ans | (1<<i);
+        }
+        return ans;
+    }
+};
