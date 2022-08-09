c09312eaa886e88dadce552f49d0bbc57d3e9c1f
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-08-10 01:12:54 +0530

    Time: 570 ms (10.11%), Space: 68.3 MB (9.70%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+     set<pair<int,int>> mp;
+     bool lcs(string &s, int i, int j, vector<vector<int>> &memo){
+        if(i > j) return 1;
+
+        if(memo[i][j] != -1) return memo[i][j];
+        bool a = 0;
+        if(s[i] == s[j]){
+            a = 1 && lcs(s, i+1, j-1, memo);
+        }
+        
+        lcs(s, i+1, j, memo);
+        lcs(s, i, j-1, memo);
+         
+        if(a){
+            if(mp.find({i,j}) == mp.end())
+                mp.insert({i,j});
+        }
+        return memo[i][j] = a;
+            
+     }
+    int countSubstrings(string s) {
+        vector<vector<int>> memo(s.size() +1, vector<int> (s.size()+1, -1));
+        lcs(s, 0, s.size()-1, memo);
+        return mp.size();
+    }
+};
