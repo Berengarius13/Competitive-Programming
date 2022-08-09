d7eff9ff8338f8db4e62af3becbf18eb3ec60ed1
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-08-10 00:54:51 +0530

    Time: 1686 ms (5.00%), Space: 284 MB (13.36%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    vector<int> ans = {0, 0, 1}; // i, j, j-i+1 aka len
+    vector<int> ans = {0, 0, 1};
     bool lcs(string &s, int i, int j, vector<vector<int>> &memo){
+        if(i > j) return 1;
+
         if(memo[i][j] != -1) return memo[i][j];
-        if(i >= j) return 1;
-        
         bool a = 0;
         if(s[i] == s[j]){
-            a = lcs(s, i+1, j-1, memo); // This considers case
+            a = 1 && lcs(s, i+1, j-1, memo);
         }
+        
         lcs(s, i+1, j, memo);
         lcs(s, i, j-1, memo);
+        if(a){
+            int len = j-i+1;
+            if(len > ans[2]){
+                ans[0] = i; ans[1] = j; ans[2] = len;
+            }
+        }
         
-        if(a == 1 && (j-i+1) > ans[2])
-            ans = {i, j, j-i+1};
-        
-        memo[i][j] = a;
-        return a;
+        return memo[i][j] = a;
+            
     }
     string longestPalindrome(string s) {
-         
-        vector<vector<int>> memo(s.size()+1, vector<int> (s.size()+1, -1));
+        vector<vector<int>> memo(s.size() +1, vector<int> (s.size()+1, -1));
         lcs(s, 0, s.size()-1, memo);
         return s.substr(ans[0], ans[2]);
     }
