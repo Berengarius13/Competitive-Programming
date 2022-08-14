3d0199b4db64abf832b81ff42e6c337d523221a8
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-08-15 04:28:52 +0530

    Time: 19 ms (84.68%), Space: 16.1 MB (56.40%) - LeetHub

--- 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.cpp
@@ -1,4 +1,9 @@
-
+/*
+* Write a returning recursion which builds solution
+* Use The mcm variation
+* Think from base case and build to the solution
+* Use bst 1 to come up with solution
+*/
 class Solution {
 public:
      vector<TreeNode*> dfs(int i, int j, vector<int> &nums){
@@ -10,9 +15,7 @@ public:
             vector<TreeNode*> right_tree = dfs(k+1, j, nums);
             for(auto left : left_tree){
                 for(auto right : right_tree){
-                    TreeNode* curr = new TreeNode(nums[k]);
-                    curr->left = left;
-                    curr->right = right;
+                    TreeNode* curr = new TreeNode(nums[k], left, right);
                     temp.push_back(curr);
                 }
             }
