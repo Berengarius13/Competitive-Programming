3a80a65cee7aa58262aa6e830fd4552c28a4da46
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-08-25 18:58:38 +0530

    Time: 2 ms (55.79%), Space: 6.6 MB (37.96%) - LeetHub

---
+++ 394-decode-string/394-decode-string.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    pair<string,int> func(string &s, int begin){
+        
+        string temp;
+        int fre = 0;
+        for(int i = begin; i < s.size(); i++){
+            if(s[i] == ']') return {temp, i};
+            if(isdigit(s[i])){
+                fre = fre*10 + (s[i]-'0'); continue;
+            }
+            if(s[i] == '['){
+                auto str_int = func(s, i+1);
+                for(int i = 0; i < fre; i++)
+                    temp += str_int.first;
+                i = str_int.second;
+                fre = 0;
+            }
+            if(s[i] != ']')
+                temp += s[i];
+        }
+        
+        return {temp, 0};
+    }
+    string decodeString(string s) {
+        return func(s, 0).first;
+    }
+};
