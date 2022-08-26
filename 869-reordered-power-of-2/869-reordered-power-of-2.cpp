9d951096071b3151c07f2a04df177a84ca10c5cd
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-08-26 15:43:18 +0530

    Time: 4 ms (61.58%), Space: 6.5 MB (29.94%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    bool ans = false;
-    void dfs(string &nums, int begin){
-        if(begin == nums.size()-1){
-            if(nums[0] == '0') return;
-            int temp = stoi(nums);
-            if((temp & (temp-1))==0) ans = true;
-            return;
-        }
-        for(int i = begin; i < nums.size(); i++){
-            swap(nums[i], nums[begin]);
-            dfs(nums, begin+1);
-            swap(nums[i], nums[begin]);
-        }
-    }
-    bool permute(string& nums) {
-        dfs(nums, 0);
-        return ans;
-    }
     bool reorderedPowerOf2(int n) {
+        unordered_set<string> s;
+        for(int i = 0; i < 31; i++){
+            long num = 1 << i;
+            string str = to_string(num);
+            sort(str.begin(), str.end());
+            s.insert(str);
+        }
         string str = to_string(n);
-        permute(str);
-        return ans;
+        sort(str.begin(), str.end());
+        return !(s.find(str) == s.end());
     }
 };
