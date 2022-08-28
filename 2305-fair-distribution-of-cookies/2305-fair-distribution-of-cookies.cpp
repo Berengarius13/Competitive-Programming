3a5621b6fcfea7b6ee680efd3f28d1da4b78d65d
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-08-28 15:28:09 +0530

    Time: 2345 ms (17.51%), Space: 6.9 MB (42.15%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -1,37 +1,25 @@
-/* *
- * DFS tree will have K nodes
- * All representing k way to fill the continer
- * DFS to get all possible subsets such that they need
-   to be divided in three parts 
-   --  --  --
-   -   --- ----
-   ---- - ---
-   All possible subset possibilities
- */
 class Solution {
 public:
     int ans = INT_MAX;
-    
-    void dfs(vector<int>& nums, int i, int &k, vector<int> &container){
+    void dfs(vector<int> &nums, int i, vector<int> &container){
         if(i == nums.size()){
-            int mx = INT_MIN;
-            for(auto &it : container)
-                mx = max(mx, it);
-            
-            ans = min(mx, ans);
+            int maxi = INT_MIN;
+            for(auto &it : container){
+                maxi = max(maxi, it);
+            }
+            ans = min(ans, maxi);
             return;
         }
         
-        for(int p = 0; p < k; p++){
-            container[p] += nums[i];
-            dfs(nums, i+1, k, container);
-            container[p] -= nums[i];
+        for(int k = 0; k < container.size(); k++){
+            container[k] += nums[i];
+            dfs(nums, i+1, container);
+            container[k] -= nums[i];
         }
-         
     }
     int distributeCookies(vector<int>& cookies, int k) {
         vector<int> container(k, 0);
-        dfs(cookies, 0,k, container);
+        dfs(cookies, 0, container);
         return ans;
     }
 };
