ee78a876b5bb5db2c9db5cb579f99977e4b8af71
Piyush <78652675+Berengarius13@users.noreply.github.com>
2022-09-08 04:16:56 +0530

    Time: 0 ms (100.00%), Space: 6 MB (46.09%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
-    double p (double x, int n){
-        if(n == 0) return 1;
-        double res = p (x , n/2);
-        if((n&1) != 0)
-            return x * res * res;
-        else
-            return res * res;
+    double ans = 1;
+    double dfs(double x, long exp, double a, long n, double ini){
+        if(n == 0){
+            ans = a*x/ini;
+            return 1;
+        }
+        if(exp*2 > n){
+            dfs(ini, 1, a*x, n-exp, ini);
+        }
+        else{
+            dfs(x*x, exp*2, a, n, ini);
+        }
+        return 1;
     }
     double myPow(double x, int n) {
-        double ans = 1;
-        if(n < 0)
-            return (1/p(x,n));
-        else
-            return p(x,n);
+        if(x == 0 && n == 0) return 1;
+        if(x == 0) return 0;
+        if(n < 0){
+            n = abs(n);
+            x = 1/x;
+        }
+        dfs(x, 1, 1, n, x);
+        return ans;
     }
 };
