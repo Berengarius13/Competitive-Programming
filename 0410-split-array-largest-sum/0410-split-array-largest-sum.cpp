bf7a4318fc00d73d0d05c897e529532867101538
Piyush <78652675+Berengarius13@users.noreply.github.com>
2023-01-10 08:59:05 +0530

    Time: 3 ms (76.55%), Space: 7.4 MB (61.60%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,38 @@
+#define ll long long int
+class Solution {
+public:
+    bool isPossible(vector<int>&n,int mid,int m){
+        int sum=0,count=1;
+        for(int i=0;i<n.size();i++){
+            sum+=n[i];
+            if(sum>mid){
+                sum=n[i];
+                count++;
+            }
+            if(count>m)return 0;
+        }
+        return 1;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        // sort(nums.begin(), nums.end());
+        int ans = 0;
+        int s = INT_MIN;
+        int e = 0;
+        for(int n:nums){
+            s = max(n,s);
+            e += n;
+        }
+        // cout<<IsPossible(nums, k, 5)<<" ";
+        while(s <= e){
+            ll mid = (s+e)/2;
+            if(isPossible(nums, mid, k)){
+                e = mid - 1;
+                ans = mid;
+            }
+            else{
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
